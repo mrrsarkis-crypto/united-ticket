5e03ee5c87aeaf5d7ba8d00fd8f4aabab74765af
mrrsarkis-crypto <intake@unitedtraffictickets.com>
2026-09-10 17:18:40 -0700

    chore: purge all advertising/consent infrastructure - site is now fully ad-free

---
+++ android-app/app/src/main/java/com/unitedtraffictickets/defense/DelegationService.java
@@ -0,0 +1,14 @@
+package com.unitedtraffictickets.defense;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
